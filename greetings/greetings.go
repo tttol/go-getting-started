3c7243608b5b40e04dc4acb3e7780ee802f8996f
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-07-08 18:44:40 +0900

    add error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// 変数宣言と代入を同時に行う`:=`
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
